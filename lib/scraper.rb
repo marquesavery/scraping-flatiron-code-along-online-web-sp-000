dd697cfdb7003067b6aab6a3dac0ca2125f11777
Marques Avery <averymarques4@gmail.com>
2020-09-01 01:03:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,6 +21,13 @@ class Scraper
     self.get_page.css(".post")
   end
 
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css(
+      
+      )
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
